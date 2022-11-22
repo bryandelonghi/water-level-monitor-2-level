eecd7f64d9c8a67aea1f83c415ef3278bffd5c55
bryandelonghi <113536458+bryandelonghi@users.noreply.github.com>
2022-11-22 15:47:23 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,23 +2,20 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("reset")
     basic.clearScreen()
     water_level = 0
-    minimum_level = 0
 })
-let minimum_level = 0
 let water_level = 0
 basic.clearScreen()
 water_level = 0
-minimum_level = 0
 basic.forever(function () {
-    if (input.pinIsPressed(TouchPin.P0)) {
+    if (input.pinIsPressed(TouchPin.P0) && (input.pinIsPressed(TouchPin.P1) && input.pinIsPressed(TouchPin.P2))) {
+        water_level = 8
+    } else if (input.pinIsPressed(TouchPin.P0) && input.pinIsPressed(TouchPin.P1)) {
+        water_level = 4
+    } else if (input.pinIsPressed(TouchPin.P0)) {
         water_level = 1
+    } else {
+        water_level = 0
     }
-    if (input.pinIsPressed(TouchPin.P0) && input.pinIsPressed(TouchPin.P1)) {
-        water_level = 3
-    }
-    if (water_level > minimum_level) {
-        water_level = water_level
-        basic.showNumber(minimum_level)
-        basic.showString("cm")
-    }
+    basic.showNumber(water_level)
+    basic.showString("cm")
 })
